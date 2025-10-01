3c357a5964b2d2803dfda4fbe4d46856f0508066
Alexander-ml <cealexmed2020@gmail.com>
2025-10-01 15:44:43 -0500

    Primeros pasos de cada tipo de carpeta, frontend y backend

---
+++ backendHackthon/src/test/java/com/hackthon/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackthon.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
